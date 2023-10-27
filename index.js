36a2f3fab872b7e46093e9bc070fb10658b8bf15
Justine George <justinegeo96@gmail.com>
2023-10-27 17:35:59 -0500

    add file upload config

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+const uploadsDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, file.originalname + "-" + Date.now() + ext);
+  },
+});
 
+const upload = multer({ storage: storage });
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  console.log(req.file);
+  const { originalname, mimetype, size } = req.file;
+  res.json({ name: originalname, type: mimetype, size: size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
